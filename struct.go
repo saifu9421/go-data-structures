b9022d1ed77611e960db4b49fd6ff71f30486f0b
saifu9421 <saifursaif905@gmail.com>
2025-01-29 21:36:57 +0600

    nestedd-struct

--- struct.go
+++ struct.go
@@ -1,15 +1,47 @@
 package main 
 import "fmt";
 
-type Clacolat func(int , int) int;
+// type Clacolat func(int , int) int;
 
-type Area struct{
-	 length int
-	 width int
-	 rect Clacolat
+// type Area struct{
+// 	 length int
+// 	 width int
+// 	 rect Clacolat
+// };
+
+  type Address struct{
+	      city string
+		  jela string
+		  upojela string
+		  zepcode int
+  };
+     
+type  Parson struct{
+	  name  string
+	  age int
+	   address Address 
 };
 
+
  func main (){
+
+	  p:= [] Parson{
+		 {
+			name:"saif",
+			age: 34,
+			address:Address{
+				"Comilla",
+				"comilla",
+				"sodor",1234},
+		},
+		 {name:"asif",age: 36,address:Address{"Dhaka","dmaaa","annujn",4554}},
+		 {name:"kamal",age: 44,address:Address{"Comilla","comilla","sodor",1234}},
+
+	  } ;
+	    for _,par :=  range p{
+			 fmt.Print(par,"\n");
+		}
+	   
 	//    type parson struct{
 	// 	 name string;
 	// 	  age int;
@@ -40,12 +72,24 @@ type Area struct{
 	//   fmt.Println(area.wieght);
   
 	   
-	 type Parson struct {
-		 fastName  string;
-		  lastName  string;
-		   age  int;
-		    cgpa float64;
-	 };
+	//  type Parson struct {
+	// 	 fastName  string;
+	// 	  lastName  string;
+	// 	   age  int;
+	// 	    cgpa float64;
+	//  };
+
+	// //  var parson1 Parson;
+	//   parson1 := [] Parson{
+	// 	     {"saif","asif",34,3.45},
+	// 		 {"saif","asif",34,3.45},
+	// 		 {"saif","asif",34,3.45},
+	//   };
+	//   fmt.Println(parson1[0].age,parson1[1].fastName);
+	//   for _,par := range parson1{
+	// 	   fmt.Println(par.fastName,par.lastName,par.cgpa,par.age);
+	//   };
+
 	    // var parson Parson;
 		//  parson = Parson{
 		// 	 fastName: "saifur",
@@ -60,20 +104,20 @@ type Area struct{
 
 
 
-	var ar  Area;
-	 ar = Area{
-		 length: 10,
-		 width: 30,
-		//   rect: func(len int , wi int) int {
-		// 	  return len*wi;
-		//   },
-		rect: func(length int, wi int) int {
-			return length * wi;
-		  },
-	 };
+	// var ar  Area;
+	//  ar = Area{
+	// 	 length: 10,
+	// 	 width: 30,
+	// 	//   rect: func(len int , wi int) int {
+	// 	// 	  return len*wi;
+	// 	//   },
+	// 	rect: func(length int, wi int) int {
+	// 		return length * wi;
+	// 	  },
+	//  };
 	//  fmt.Println(ar);
 	//   fmt.Println(ar.rect(ar.length,ar.width));
-	fmt.Println(ar.rect(ar.length,ar.width));
+// 	fmt.Println(ar.rect(ar.length,ar.width));
 
  }
 
